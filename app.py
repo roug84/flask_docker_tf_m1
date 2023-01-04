3b004a8c68a5cbe4cfa38a55d9a7f55201d419db
Hector <hector.m.romero.ugalde@gmail.com>
2023-01-04 23:56:33 +0100

    Starting project

---
+++ app.py
@@ -0,0 +1,21 @@
+"""
+Flask running blueprints
+"""
+from flask import Flask
+import os
+from dotenv import load_dotenv
+from views.index import bp as index_bp
+from views.upload_file import bp10 as upload_file_bp
+
+load_dotenv()
+app = Flask(__name__)
+name = os.getenv('NAME')
+
+# Register BPs
+app.register_blueprint(index_bp)
+app.register_blueprint(upload_file_bp)
+
+if __name__ == '__main__':
+    # app.secret_key = os.urandom(12)
+    #app.run(debug = True)
+    app.run(port='8000')
