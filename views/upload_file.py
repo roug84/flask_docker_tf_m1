c571c5c1d85331da3140390fca413543464c44b4
Hector Romero-Ugalde <hector@diabeloop.frhector@diabeloop.frhector@diabeloop.fr>
2023-01-08 23:10:39 +0100

    using adapter

--- views/upload_file.py
+++ views/upload_file.py
@@ -101,7 +101,7 @@ def view():
             session = requests.Session()
             retry = Retry(connect=3, backoff_factor=0.5)
             adapter = HTTPAdapter(max_retries=retry)
-            # session.mount('http://', adapter)
+            session.mount('http://', adapter)
             # session.mount('https://', adapter)
             session.trust_env = False
             # response = session.get(url, json=my_json)
